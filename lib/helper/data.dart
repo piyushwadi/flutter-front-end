123f0c82005bd2e8831e2fe2962903b3abbc4b6d
sonishreyas <sonishreyas10@gmail.com>
2021-09-26 17:52:34 +0530

    changed to taake data from json file

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -18,10 +18,10 @@ List<CategorieModel> getCategories(){
   myCategories.add(categorieModel);
 
   //3
-  categorieModel = new CategorieModel();
-  categorieModel.categorieName = "General";
-  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
-  myCategories.add(categorieModel);
+  // categorieModel = new CategorieModel();
+  // categorieModel.categorieName = "General";
+  // categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  // myCategories.add(categorieModel);
 
   //4
   categorieModel = new CategorieModel();
@@ -42,10 +42,10 @@ List<CategorieModel> getCategories(){
   myCategories.add(categorieModel);
 
   //5
-  categorieModel = new CategorieModel();
-  categorieModel.categorieName = "Technology";
-  categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
-  myCategories.add(categorieModel);
+  // categorieModel = new CategorieModel();
+  // categorieModel.categorieName = "Technology";
+  // categorieModel.imageAssetUrl = "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  // myCategories.add(categorieModel);
 
   return myCategories;
 
--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,9 +1,10 @@
+import 'dart:ffi';
+
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:fluttertry2/models/article.dart';
 import 'dart:convert';
-
 import 'package:get_it/get_it.dart';
-
 import '../main.dart';
 
 GetIt locator = GetIt.instance;
@@ -11,19 +12,23 @@ var appInfo = locator<LanguageService>();
 
 class News {
   List<Article> news = [];
-
+  Future<JsonCodec> readJson() async {
+      final String response = await rootBundle.loadString('assets/news.json');
+      var data = await json.decode(response);
+      return data;
+  }
   Future<void> getNews() async {
     // print("Application language set to " + appInfo.current);
 
-    String url = "http://34.70.13.196:80/get_data";
+    // String url = "http://34.70.13.196:80/get_data?topic=Latest+News";
 
-    var response = await http.get(url);
-
-    var jsonData = jsonDecode(response.body);
+    // var response = await http.get(url);
+    var jsonData = readJson();
+    // var jsonData = jsonDecode(response.body);
 
     if (true) {
       jsonData.forEach((element) {
-        if (element['summary'] != null) {
+        if (element['summary'] != null && element['topic'] == "Latest News") {
           Article article = Article(
               title: element['title'],
               summary: element['summary'],
@@ -38,11 +43,11 @@ class News {
   Future<void> getNewsLanguage(String choice) async {
     // print("Application language set to " + appInfo.current);
 
-    String url = "http://34.70.13.196:80/get_data?language=$choice";
+    // String url = "http://34.70.13.196:80/get_data?language=$choice&topic=Latest+News";
     news = [];
-    var response = await http.get(url);
-
-    var jsonData = jsonDecode(response.body);
+    // var response = await http.get(url);
+    var jsonData = readJson();
+    // var jsonData = jsonDecode(response.body);
     if (choice == "English") {
       choice = "";
     } else {
@@ -51,7 +56,7 @@ class News {
 
     if (true) {
       jsonData.forEach((element) {
-        if (element[choice + 'summary'] != null) {
+        if (element[choice + 'summary'] != null  && element['topic'] == "Latest News") {
           Article article = Article(
               title: element[choice + 'title'],
               summary: element[choice + 'summary'],
@@ -75,14 +80,14 @@ class NewsForCategorie {
     // entertainment
     // Entertainment
 
-    String url = "http://34.70.13.196:80/get_data?topic=$category";
+    // String url = "http://34.70.13.196:80/get_data?topic=$category";
     // String url = "http://34.70.13.196:80/get_data?topic=Entertainment";
 
-    var response = await http.get(url);
+    // var response = await http.get(url);
 
-    var jsonData = jsonDecode(response.body);
+    // var jsonData = jsonDecode(response.body);
     // print(url);
-
+    var jsonData = readJson();
     if (lang == "English") {
       lang = "";
     } else {
@@ -91,7 +96,7 @@ class NewsForCategorie {
 
     if (true) {
       jsonData.forEach((element) {
-        if (element[lang + 'summary'] != null) {
+        if (element[lang + 'summary'] != null && element['topic'] == category {
           Article article = Article(
               title: element[lang + 'title'],
               summary: element[lang + 'summary'],
